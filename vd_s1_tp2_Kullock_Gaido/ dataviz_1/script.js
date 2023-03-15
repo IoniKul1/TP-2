9f10974e740df509e73837e1c9e5d887370d883e
IoniKul1 <ykullock@mail.utdt.edu>
2023-03-15 11:26:49 -0300

    primer graph

--- vd_s1_tp2_Kullock_Gaido/ dataviz_1/script.js
+++ vd_s1_tp2_Kullock_Gaido/ dataviz_1/script.js
@@ -2,12 +2,18 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
   console.log(data)
   // Guardamos el svg generado en la variable chart
   let chart = Plot.plot({
-    marks: [
-      Plot.dot(data, {
-        x: 'fertility',
-      }),
-    ],
+    marks:[
+        Plot.barY(
+          data,
+          Plot.groupX({ y: "mean" }, { x: "edad_mision", y: "mision_hs", fill: 'red'}) )],
+    grid: true,
+    line: true,
+    nice: true,
+    label: '                                                                                           Edad optima para tu proxima misión',
+    height: 500,
+    marginLeft: 100,
   })
   // Agregamos chart al div#chart de index.html
   d3.select('#chart').append(() => chart)
+  
 })
