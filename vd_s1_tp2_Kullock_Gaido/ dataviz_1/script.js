d3bde974dd83b2c6dc28b2c4c0fb14f86a657eed
IoniKul1 <ykullock@mail.utdt.edu>
2023-03-15 11:55:51 -0300

    pushh1

--- vd_s1_tp2_Kullock_Gaido/ dataviz_1/script.js
+++ vd_s1_tp2_Kullock_Gaido/ dataviz_1/script.js
@@ -6,12 +6,23 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
         Plot.barY(
           data,
           Plot.groupX({ y: "mean" }, { x: "edad_mision", y: "mision_hs", fill: 'red'}) )],
+    style: {
+      backgroundColor: "black",
+      color: "white",
+      fontFamily: "monospace",
+      fontSize: "12px",
+      overflow: "visible"
+    },
     grid: true,
     line: true,
     nice: true,
     label: 'Edad optima para tu proxima misión',
     height: 500,
+    width: 1000,
     marginLeft: 100,
+    marginTop: 40,
+    marginBottom: 40,
+  
   })
   // Agregamos chart al div#chart de index.html
   d3.select('#chart').append(() => chart)
