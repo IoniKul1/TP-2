b8928c5618f180bdcf6a8b7cfdc907f63f7cced0
IoniKul1 <ykullock@mail.utdt.edu>
2023-03-15 10:12:00 -0300

    tnopvto

--- vd_s1_tp2_Kullock_Gaido/ dataviz_1/script.js
+++ vd_s1_tp2_Kullock_Gaido/ dataviz_1/script.js
@@ -1,6 +1,5 @@
-d3.csv('data2000.csv', d3.autoType).then(data => {
+d3.csv('astronautas.csv', d3.autoType).then(data => {
   console.log(data)
-  console.log('hola ioni')
   // Guardamos el svg generado en la variable chart
   let chart = Plot.plot({
     marks: [
