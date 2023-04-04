e39cdc34b89655b1edc154b7369416f7c02e701d
IoniKul1 <ykullock@mail.utdt.edu>
2023-04-04 18:50:28 -0300

    sq

--- vd_s1_tp2_Kullock_Gaido/exploracion/Charts/chart2.js
+++ vd_s1_tp2_Kullock_Gaido/exploracion/Charts/chart2.js
@@ -1,49 +1,53 @@
 d3.csv('astronautass.csv', d3.autoType).then((data) => {
-  let chart2 = Plot.plot({
-    marks: [
-      Plot.barY(
-        data,
-        Plot.groupX({ y: "count" },
-         { x: "ocupacion", 
-         y: "", 
-         fill:"genero",
-         sort: { x: "y", reverse: true },
-          })
-      )
-    ],
-    color: {
-      legend: false,
-      scheme: 'blues',
-    },
-  grid:true,
-  line:true,
-  nice:true,
-  width: 780,
-  height: 500,
-  style: {
-    color: "white",
-    fontFamily: "Helvetica",
-    fontSize: "12px",
-    overflow: "visible",
-    background: "transparent",
-  },
-   marginLeft: 55,
-	 marginRight: 55,
-	 marginTop: 50,
-	 marginBottom: 130,
-	 insetTop: 10,
-	 insetBottom: 10,
-	 insetLeft:10,
-	 insetRight: 10,
-   x: {
-    label:"OCUPACIONES",
-    tickRotate: -30,
-    tickSize: 10,
-   },
-   y: {
-    label:"CANTIDAD DE OCUPANTES",
-   }
-  });
+  const filteredData = data.filter(d => d.nacionalidad === 'EE.UU.' || d.nacionalidad === 'U.S.S.R/Rusia'|| d.nacionalidad === 'Francia'|| d.nacionalidad === 'Reino Unido' || d.nacionalidad === 'Emiratos Arabes Unidos'
+  );
+  
+    
+          let chart2 = Plot.plot({
+            marks: [
+              Plot.barY(
+                filteredData,
+                Plot.groupX({ y: "count" },
+                 { x: "ocupacion", 
+                 y: "", 
+                 fill:"nacionalidad",
+                 sort: { x: "y", reverse: true },
+                }) ),
+          
+      ],
+      color: {
+        legend: false,
+        
+      },
+    grid:true,
+    line:true,
+    nice:true,
+    width: 780,
+    height: 500,
+    style: {
+      color: "white",
+      fontFamily: "helvetica",
+      fontSize: "12px",
+      background: "transparent",
+      
+     },
+     marginLeft: 55,
+     marginRight: 55,
+     marginTop: 50,
+     marginBottom: 130,
+     insetTop: 10,
+     insetBottom: 10,
+     insetLeft:10,
+     insetRight: 10,
+     x: {
+      label:"OCUPACIONES",
+      tickRotate: -30,
+      tickSize: 10,
+     },
+     y: {
+      label:"CANTIDAD DE OCUPANTES",
+     }
+    });
   
   d3.select("#chart2").append(() => chart2);
 
